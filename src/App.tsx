267f17511c36cfe4931ba86abf56db6f2e0bbf27
trsaints <thiago.dev2k3@gmail.com>
2024-03-31 09:14:34 -0300

    feat: create base terrarium structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="page">
+      <h1>My Terrarium</h1>
+
+      <div id="left-container" className="container">
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant1" src="/plant1.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant2" src="/plant2.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant3" src="/plant3.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant4" src="/plant4.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant5" src="/plant5.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant6" src="/plant6.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant7" src="/plant7.png" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div id="right-container" className="container">
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant8" src="/plant8.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant9" src="/plant9.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant10" src="/plant10.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant11" src="/plant11.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant12" src="/plant12.png" />
+        </div>
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant13" src="/plant13.png" />
+        </div>
+
+        <div className="plant-holder">
+          <img className="plant" alt="plant" id="plant14" src="/plant14.png" />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div id="terrarium">
+        <div className="jar-top"></div>
+        <div className="jar-walls">
+          <div className="jar-glossy-long"></div>
+          <div className="jar-glossy-short"></div>
+        </div>
+        <div className="dirt"></div>
+        <div className="jar-bottom"></div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
